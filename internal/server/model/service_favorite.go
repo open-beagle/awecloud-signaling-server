89c2767fa718173fec21e66440a105d846de1388
shucheng <shucheng@bd-apaas.com>
2025-12-04 22:58:44 +0800

    fix fav bug

--- internal/server/model/service_favorite.go
+++ internal/server/model/service_favorite.go
@@ -7,7 +7,7 @@ import (
 // ServiceFavorite 服务收藏表
 type ServiceFavorite struct {
 	ID             int64     `gorm:"primaryKey" json:"id"`
-	ClientID       int64     `gorm:"not null;index:idx_service_favorites_client_id" json:"client_id"`
+	ClientID       int64     `gorm:"not null;index:idx_service_favorites_client_id;uniqueIndex:idx_client_instance" json:"client_id"`
 	STCPInstanceID int64     `gorm:"not null;index:idx_service_favorites_instance_id;uniqueIndex:idx_client_instance" json:"stcp_instance_id"`
 	LocalPort      int       `gorm:"default:0" json:"local_port"` // 用户偏好的本地端口
 	CreatedAt      time.Time `gorm:"autoCreateTime" json:"created_at"`
